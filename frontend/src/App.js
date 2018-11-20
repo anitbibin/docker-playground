03e8e97c631df4da66b0e5767f9dc07e21654efc
Anit <anitbibin1202@gmail.com>
2018-11-20 14:35:58 +0800

    check travis ci

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React here yes.
+            Learn React here - check travis CI.
           </a>
         </header>
       </div>
